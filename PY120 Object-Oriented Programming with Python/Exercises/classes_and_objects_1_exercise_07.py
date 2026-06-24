a546bcd99d9936ef321445d5fcc30e823a9963cd
Praveen Bagra <prvnbgda@gmail.com>
2026-06-24 22:00:14 +0530

    Added files.

---
+++ PY120 Object-Oriented Programming with Python/Exercises/classes_and_objects_1_exercise_07.py
@@ -0,0 +1,10 @@
+class Cat:
+
+    def __init__(self, name):
+        self.__name = name
+
+    def greet(self):
+        print(f'Hello! My name is {self.__name}!')
+
+kitty = Cat('Sophie')
+kitty.greet()
