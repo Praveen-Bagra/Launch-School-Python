2fd2b7aaba332bdb38ceab16021d14ac57df1e27
Praveen Bagra <prvnbgda@gmail.com>
2026-06-27 19:34:01 +0530

    Added files.

---
+++ PY120 Object-Oriented Programming with Python/Exercises/inheritance_and_mixins_exercise_07.py
@@ -0,0 +1,26 @@
+class TowingMixin:
+    def tow(self):
+        return 'I can tow a trailer!'
+
+class Vehicle:
+
+    def __init__(self, year):
+        self.year = year
+
+class Truck(TowingMixin, Vehicle):
+    
+    def __init__(self, year):
+        super().__init__(year)
+
+class Car(Vehicle):
+
+    def __init__(self, year):
+        super().__init__(year)
+
+# Comments show expected output
+truck1 = Truck(1994)
+print(truck1.year)            # 1994
+print(truck1.tow())           # I can tow a trailer!
+
+car1 = Car(2006)
+print(car1.year)              # 2006
