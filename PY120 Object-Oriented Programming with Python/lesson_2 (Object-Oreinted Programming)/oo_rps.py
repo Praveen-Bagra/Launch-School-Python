0004f2bdbba8ad07090b543f456880734d436756
Praveen Kumar Bagra <prvnbgda@gmail.com>
2026-06-30 23:41:26 +0530

    Added files.

--- PY120 Object-Oriented Programming with Python/lesson_2 (Object-Oreinted Programming)/oo_rps.py
+++ PY120 Object-Oriented Programming with Python/lesson_2 (Object-Oreinted Programming)/oo_rps.py
@@ -1,5 +1,5 @@
 # Rock Paper Scissors is a two-player game where each player chooses
-# one of three possible moves: rock, paper, or scissors. The choses moves
+# one of three possible moves: rock, paper, or scissors. The chosen moves
 # will then be compared to see who wins, according to the following
 # rules:
 
@@ -24,48 +24,37 @@ import random
 class Player:
     CHOICES = ('rock', 'paper', 'scissors')
 
-    def __init__(self, player_type):
-        self._player_type = player_type.lower()
+    def __init__(self):
         self.move = None
 
-    def _is_human(self):
-        return self._player_type == 'human'
+class Computer(Player):
+    def __init__(self):
+        super().__init__()
 
     def choose(self):
-        if self._is_human():
-            prompt = 'Please choose rock, paper, or scissors: '
+        self.move = random.choice(Player.CHOICES)
 
-            while True:
-                choice = input(prompt).lower()
-                if choice in Player.CHOICES:
-                    break
-
-                print(f'Sorry, {choice} is not valid')
-
-            self.move = choice
-        else:
-            self.move = random.choice(Player.CHOICES)
-
-class Move:
+class Human(Player):
     def __init__(self):
-        # This seems like we need something to keep track
-        # of the choice... a move object can be "paper", "rock", "scissors"
-        pass
+        super().__init__()
 
-class Rule:
-    def __init__(self):
-        # not sure what the "state" of a rule object should be
-        pass
+    def choose(self):
+        prompt = 'Please choose rock, paper, or scissors: '
 
-    # not sure where "compare" goes yet
-    def compare(self, move1, move2):
-        pass
+        while True:
+            choice = input(prompt).lower()
+            if choice.lower() in Player.CHOICES:
+                break
+
+            print(f'Sorry, {choice} is not valid')
+
+        self.move = choice
 
 class RPSGame:
     def __init__(self):
-        self._human = Player('human')
-        self._computer = Player('computer')
-    
+        self._human = Human()
+        self._computer = Computer()
+
     def _display_welcome_message(self):
         print('Welcome to Rock Paper Scissors!')
 
@@ -73,39 +62,46 @@ class RPSGame:
         print('Thanks for playing Rock Paper Scissors. Goodbye!')
 
     def _display_winner(self):
-        human_move = self._human.move
-        computer_move = self._computer.move
-
         print(f'You chose: {self._human.move}')
         print(f'The computer chose: {self._computer.move}')
 
-        if ((human_move == 'rock' and computer_move == 'scissors') or
-            (human_move == 'paper' and computer_move == 'rock') or
-            (human_move == 'scissors' and computer_move == 'paper')):
-
+        if self._human_wins():
             print('You win!')
-        elif ((computer_move == 'rock' and human_move == 'scissors') or
-            (computer_move == 'paper' and human_move == 'rock') or
-            (computer_move == 'scissors' and human_move == 'paper')):
-
+        elif self._computer_wins():
             print('Computer wins!')
         else:
             print("It's a tie")
 
-    def _play_again(self):
-        answer = input('Would you like to play again? (y/n) ')
-        return answer.lower().startswith('y')
+    def _human_wins(self):
+        human_move = self._human.move
+        computer_move = self._computer.move
+
+        return ((human_move == 'rock' and computer_move == 'scissors') or
+                (human_move == 'paper' and computer_move == 'rock') or
+                (human_move == 'scissors' and computer_move == 'paper'))
+
+    def _computer_wins(self):
+        human_move = self._human.move
+        computer_move = self._computer.move
+
+        return ((computer_move == 'rock' and human_move == 'scissors') or
+                (computer_move == 'paper' and human_move == 'rock') or
+                (computer_move == 'scissors' and human_move == 'paper'))
 
     def play(self):
         self._display_welcome_message()
-        
+
         while True:
             self._human.choose()
             self._computer.choose()
             self._display_winner()
             if not self._play_again():
                 break
-            
+
         self._display_goodbye_message()
+
+    def _play_again(self):
+        answer = input("Would you like to play again? (y/n) ")
+        return answer.lower().startswith('y')
 
 RPSGame().play()
